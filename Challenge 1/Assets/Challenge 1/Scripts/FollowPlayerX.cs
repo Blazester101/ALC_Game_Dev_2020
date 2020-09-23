7878f934d4f097486ff39f9f02dcfc7e70651ed1
Tim Olsen <blazester101@gmail.com>
2020-09-23 16:53:01 -0600

    Made  camera follow plane and made propeller spin

--- Challenge 1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Challenge 1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset = new Vector3(5, 1, 0);
+    public Vector3 offset = new Vector3(15, 2, 0);
 
     // Start is called before the first frame update
     void Start()
