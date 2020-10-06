1794d255eb46ae415ec95b15f355877348598321
Tim Olsen <blazester101@gmail.com>
2020-10-06 09:10:46 -0600

    Edited wine bottle and create better player movement

--- Prototype 2 - Feed the Animals/Assets/Course Library/Scripts/PlayerController.cs
+++ Prototype 2 - Feed the Animals/Assets/Course Library/Scripts/PlayerController.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float horizontalInput;
+    public float speed = 20.0f;
+    public float bounds = 24.0f;
+    private float horizontalInput;
     
     // Start is called before the first frame update
     void Start()
@@ -15,8 +17,20 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Collerct input data
         horizontalInput = Input.GetAxis("Horizontal");
         
-        transform.Translate(Vector3.right * /*Time.deltaTime **/ horizontalInput);
+        // Moves player left and right
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+
+        // Adds bounds to the right and left
+        if(transform.position.x < -bounds)
+        {
+            transform.position = new Vector3(-bounds,transform.position.y,transform.position.z);
+        }
+        if(transform.position.x > bounds)
+        {
+            transform.position = new Vector3(bounds,transform.position.y,transform.position.z);
+        }
     }
 }
