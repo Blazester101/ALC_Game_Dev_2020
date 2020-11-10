2d7394343949c55081008fe1196b5f3ec9f25a33
Tim Olsen <blazester101@gmail.com>
2020-11-10 09:10:26 -0700

    Began adding animation

--- Prototype 3 - Jump Force/Assets/Scripts/PlayerController.cs
+++ Prototype 3 - Jump Force/Assets/Scripts/PlayerController.cs
@@ -9,12 +9,15 @@ public class PlayerController : MonoBehaviour
     public float gravityMod;
     public bool isOnGround = true;
     public bool gameOver = false;
+
+    private Animator playerAnim;
     
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityMod;
+        playerAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,6 +27,7 @@ public class PlayerController : MonoBehaviour
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
 
