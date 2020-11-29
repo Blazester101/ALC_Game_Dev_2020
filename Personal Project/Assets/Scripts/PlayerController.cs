9d9ac6643d2896677fb7ab973aa569cf6c8147ea
Tim Olsen <blazester101@gmail.com>
2020-11-29 15:36:13 -0700

    Moved camera and added player movement

---
+++ Personal Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 25.0f;
+    
+    private float horizontalInput;
+    private float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        
+        // Moves the player forward and back based on verticalInput
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        // Moves the player left and right based on horizontalInput
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+    }
+}
