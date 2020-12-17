b65c1d6f402b92758e623b0b4a8f73c6f349ddea
Tim Olsen <blazester101@gmail.com>
2020-12-17 16:02:11 -0700

    Finished 4.2

--- Prototype 4 - Sumo Battle/Assets/Scripts/RotateCamera.cs
+++ Prototype 4 - Sumo Battle/Assets/Scripts/RotateCamera.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class RotateCamera : MonoBehaviour
 {
-    public float rotationSpeed = 40;
+    public float rotationSpeed = 60;
 
     // Start is called before the first frame update
     void Start()
