3185b7ccc772552e3a5dcb95416e2f8c4fcb05a8
Tim Olsen <blazester101@gmail.com>
2021-01-05 17:46:03 -0700

    Finished 5.2

--- Prototype 5 - Clicky Mouse/Assets/Scripts/Target.cs
+++ Prototype 5 - Clicky Mouse/Assets/Scripts/Target.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
+    private GameManager gameManager;
+    public ParticleSystem explosionParticle;
 
     private float minSpeed = 12.0f;
     private float maxSpeed = 16.0f;
@@ -13,10 +15,13 @@ public class Target : MonoBehaviour
     private float xRange = 4.0f;
     private float ySpawnPos = -2.0f;
 
+    public int pointValue;
+
     // Start is called before the first frame update
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
 
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
@@ -33,6 +38,8 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
     }
 
     private void OnTriggerEnter(Collider other)
